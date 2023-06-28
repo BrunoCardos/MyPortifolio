48f9b5949d8975f08a083f62ae17ee69612b12df
BrunoCardos <brunocardoso@sapo.cv>
2023-06-28 11:34:10 -0100

    Add Form funtionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,12 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
+// lOb1Y2bGkBJhKcA_9
+// template_6ruqpof
+// service_g8qmj0u
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -15,11 +21,50 @@ const Contact = () => {
     message: '',
   })
 
-  const [Loading, setLoding] = useState(false)
+  const [Loading, setLoading] = useState(false)
 
-  const handleChange = (e) => { }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => { }
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_g8qmj0u",
+        "template_6ruqpof",
+        {
+          from_name: form.name,
+          to_name: "Bruno Cardoso",
+          from_email: form.email,
+          to_email: "brunocardoso@sapo.cv",
+          message: form.message,
+        },
+        "lOb1Y2bGkBJhKcA_9"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
