8479fa20675ed95b6057a076e5d02fe084d8863a
BrunoCardos <brunocardoso@sapo.cv>
2023-06-28 13:21:13 -0100

    Change the content of Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Bruno</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Full-Stack developer skilled in front-end  <br className='sm:block hidden' />
+            and back-end technologies, delivering robust web applications with efficiency and user-centric design.
           </p>
         </div>
       </div>
